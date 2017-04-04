aea85841dfec5731a0a162c257487de1e63f33d1
Gabriel Morales <grmorale@gmail.com>
2017-04-03 18:22:37 -0700

    Allow players to save and load games

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ The computer loads a dictionary of words and selects a word between 5 and 12 let
 Players must guess the word, one letter at a time, before their chances run out.
 
 Each turn, the game displays the word in progress and incorrectly guessed letters.
+
+At the start of the game, users may load an old game.
+
+Each turn, players may save their progress.
--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,11 @@
 require "csv"
+require "yaml"
 
 class Hangman
 
   def initialize
-    list=File.readlines "5desk.csv"
+    list=File.readlines "5desk.csv"    
+    ask_to_load
     @word=list.sample    
     while @word.length<5 || @word.length>12
       @word=list.sample
@@ -18,12 +20,49 @@ class Hangman
     gameplay
   end
 
+  def ask_to_load
+    puts "Want to load your previous game? Y/N"
+    choice=gets.chomp
+    if choice.upcase=="Y"
+      load
+      gameplay
+    else
+      initialize
+    end
+  end
+
   def gameplay
     while @display_word.include? "_"
     display
+    puts "Want to save this game? Y/N"
+    choice=gets.chomp
+    if choice.upcase=="Y"
+      save
+      guess
+    else
     guess
     end
+    end
     puts "Congratulations. The word was #{@word}"
+  end
+
+  def save
+    saved_game={ :word=>@word,
+      :display_word=>@display_word,
+      :wrong_letters=>@wrong_letters,
+      :counter=>@counter
+    }
+    File.open("save-file.yml","r+") do |f|
+      f.write(saved_game.to_yaml)
+    end
+  end
+
+  def load
+    loaded_game=YAML.load_file("save-file.yml")
+    @word=loaded_game[:word]
+    @display_word=loaded_game[:display_word]
+    @wrong_letters=loaded_game[:wrong_letters]
+    @counter=loaded_game[:counter]
   end
 
   def display
@@ -34,6 +73,7 @@ class Hangman
   end
 
   def guess
+    #bug occurs with the first letter checking a capital
     puts "Guess a letter"
     user_guess=gets.chomp
     puts "You guessed #{user_guess}"
